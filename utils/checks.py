cfceb4c91452487d9d5ed984f9a776ed8ba81029
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-04-08 22:10:02 +0100

    Create checks.py

---
+++ utils/checks.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+
+async def check_guild_permissions(ctx, perms, *, check=all):
+    is_owner = await ctx.bot.is_owner(ctx.author)
+    if is_owner:
+        return True
+
+    if ctx.guild is None:
+        return False
+
+    resolved = ctx.author.guild_permissions
+    return check(getattr(resolved, name, None) == value for name, value in perms.items())
+
+def is_mod(perm):
+    async def pred(ctx):
+        return await check_guild_permissions(ctx, {perm: True})
+    
+    return commands.check(pred)
+
+def is_banned(ctx):
+    if ctx.author.id in ctx.bot.ban_cache:
+        return True
+    
+    return False
