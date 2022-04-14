b781164c54206bedaa3620feac10de5cb0d4d2ae
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-04-15 00:32:44 +0100

    make values an arg and await execution/fetch
    not a kwarg

--- utils/db.py
+++ utils/db.py
@@ -1,32 +1,7 @@
 from discord.ext import commands
 
-def execute(bot: commands.Bot, query: str, **values):
-    bot.conn.execute(query, **values)
+async def execute(bot: commands.Bot, query: str, *values):
+    return await bot.conn.execute(query, *values)
 
-def fetch(bot: commands.Bot, query: str, **values):
-    bot.conn.fetch(query, **values)
-
-def create_tables():
-    query = """CREATE TABLE public.prefixes
-    (
-        guild_id bigint NOT NULL,
-        prefix character varying NOT NULL,
-        PRIMARY KEY (guild_id)
-    )
-    WITH (
-        OIDS = FALSE
-    );
-
-    CREATE TABLE public.prefixes
-    (
-        guild_id bigint NOT NULL,
-        prefix character varying NOT NULL,
-        PRIMARY KEY (guild_id)
-    )
-    WITH (
-        OIDS = FALSE
-    );
-
-    ALTER TABLE IF EXISTS public.prefixes, public.botbans
-    OWNER to postgres;
-    """
+async def fetch(bot: commands.Bot, query: str, *values):
+    return await bot.conn.fetch(query, *values)
